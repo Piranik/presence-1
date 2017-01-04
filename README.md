0bf3acfcec472081cca5ca6373c9889deb34c326
fryguy04 <fryguy04@gmail.com>
2017-01-04 02:08:02 -0500

    Major telegram updates, TG can read/update YAML MAC Database and /list

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# presence
-Scan local network for new MAC addr. Log and notify
+This project is really two main components: 1) Ping sweep detection of new Hosts on your network and 2) Telegram notification bot to let you know a new host appeared ASAP.
+
+Ping sweep (NMAP) local network keeping track of known hosts based on MAC. When a new MAC addresses is detected, log the IP/MAC and MAC Vendor to YAML file and notify me via Telegram bot.
+
+When admin receives a text message he can reply to the Bot to give that new host a proper name. The thought is that you receive the update in near real-time and you know who/what was just plugged or Wifi attached to your network so you name it right away.
+
+TODO:
+* Telegram bot will allow Admin to Block (future) that MAC via Firewall rule
+* Log and visually graph when hosts are online vs offline historically
+* possible notifications if 'required' hosts aren't online
+* Set temporary notifications when a device comes/goes (good for iPhone/person presence tracking)
+* add additional methods to detect via Bluetooth and sniff wifi for devices that are close but not attached to your network
--- telegrambot.py
+++ telegrambot.py
@@ -4,7 +4,7 @@ import telegram
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 import logging
 import yaml
-
+import re
 
 # http://telegram.me/homepresencebot
 
@@ -12,7 +12,8 @@ def send_to_telegram(txt, telegram_token, chat_id):
     bot = telegram.Bot(token=telegram_token)
     bot.sendMessage(chat_id=chat_id, text=txt)
 
-# Sets an "accept" "block" keyboard for this chat
+# not used
+# Sets an "accept" "block" custom keyboard for this chat
 def set_custom_keyboard(telegram_token, chat_id ):
     # custom_keyboard = [['top-left', 'top-right'],
     #                     ['bottom-left', 'bottom-right']]
@@ -25,25 +26,70 @@ def set_custom_keyboard(telegram_token, chat_id ):
                       text="Custom Keyboard Test",
                       reply_markup=reply_markup)
 
-def junk():
-    #print "Logged into: ", bot.getMe()['first_name']
-
-    for msg in bot.getUpdates():
-        print msg.message.chat_id
-        #print yaml.dump(msg.message, default_flow_style=False, default_style='')
-        print
-
-
-    chat_id = bot.getUpdates()[-1].message.chat_id
-    chat_id = 325815706
-
-    bot.sendMessage(chat_id=chat_id, text="Received")
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a Bot, please chat with me")
 
 def echo(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+    mac = new_description = None
+
+    # To modify database/yaml either accept:
+    #  1) a reply to a Bot post saying new MAC Found
+    #  2) message formatted: MAC New_Description
+
+    # Checking Case 2
+    match = re.match(r"(^\s*[\dA-Fa-f:]{17})\s*(.*)", update.message.text)  # Match mac addr ex: 58:55:ca:0d:28:XX New description
+    if match and len(match.groups()) == 2:
+        mac = match.group(1).upper()
+        new_description = match.group(2)
+
+    # Checking Case 1
+    try:
+        reply_original = update.message['reply_to_message']['text']
+        #print "**REPLY:", reply_original
+        # match = re.match(r"^\s([\dA-Fa-f:]{17})")  # Match mac addr ex: 58:55:ca:0d:28:XX
+        match = re.match(r"(^\s*[\dA-Fa-f:]{17})", reply_original)  # Match mac addr ex: 58:55:ca:0d:28:XX
+        if match:
+            mac = match.group(1).upper()
+            new_description = update.message.text
+    except:
+        pass
+
+    if mac and new_description:
+        update_yaml(mac, new_description)
+        bot.sendMessage(chat_id=update.message.chat_id, text="Updating %s with description: %s" % (mac, new_description))
+    else:
+        bot.sendMessage(chat_id=update.message.chat_id, text="Can't hear ya")
+
+def update_yaml(mac, new_description):
+    # Convert to ASCII since telegram uses unicode
+    mac = mac.encode('ascii','ignore')
+    new_description = new_description.encode('ascii','ignore')
+
+    print "Updating %s with description: %s" % (mac, new_description)
+
+    hosts = load_known_hosts('known_hosts.yml')
+
+    # Remove mac from "seen" if existings
+    if mac in hosts['seen']:
+        del hosts['seen'][mac]
+
+    # Remove mac from "known" if existings
+    if mac in hosts['known']:
+        del hosts['known'][mac]
+
+    # Add or overwrite mac in 'known' section
+    hosts['known'][mac] = new_description
+
+    # Write out the file to save it
+    write_yaml_hosts(hosts, hosts_file)
+
+def list_hosts(bot, update):
+    hosts = load_known_hosts(hosts_file)
+    hosts_pretty = yaml.dump(hosts, default_flow_style=False)
+    print hosts_pretty
+
+    bot.sendMessage(chat_id=update.message.chat_id, text=hosts_pretty)
 
 def caps(bot, update, args):
     text_caps = ' '.join(args).upper()
@@ -58,12 +104,16 @@ def listen(telegram_token):
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
 
+    # TODO add security handler to drop unauthorized users
+
     start_handler = CommandHandler('start', start)
+    list_handler = CommandHandler('list', list_hosts)
     echo_handler = MessageHandler(Filters.text, echo)
     caps_handler = CommandHandler('caps', caps, pass_args=True)
     unknown_handler = MessageHandler(Filters.command, unknown)
 
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(list_handler)
     dispatcher.add_handler(echo_handler)
     dispatcher.add_handler(caps_handler)
     dispatcher.add_handler(unknown_handler)
@@ -71,10 +121,22 @@ def listen(telegram_token):
     updater.start_polling()
     updater.idle()
 
+def write_yaml_hosts(hosts, hosts_file):
+    with open(hosts_file, 'w') as outfile:
+        yaml.dump(hosts, outfile, default_flow_style=False)
+
+def load_known_hosts(yamlfile):
+    with open(yamlfile, 'r') as readfile:
+        cfg = yaml.load(readfile)
+    return cfg
+
 if __name__ == '__main__':
 
-    with open(config_file, 'r') as readfile:
-        cfg = yaml.load(readfile)
+    hosts_file  = 'known_hosts.yml'
+    config_file = 'config.yml'
+
+    cfg   = load_known_hosts(config_file)
+    hosts = load_known_hosts(hosts_file)
 
     listen(cfg['telegram_token'])
 
